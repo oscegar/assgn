cfd83333f9d92397e4f9b1e0aad73014a9d91f85
oscegar <115582621+oscegar@users.noreply.github.com>
2022-11-28 20:30:14 -0800

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,45 @@ def multi_sort(arr, cmp, method="None"):
 
 
 
-# must be in-place sort
+def gaps(gap):
+    if gap <=1:
+        return 0
+
+    return int(math.ceil(gap/2))
+def swap(arr, i,j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j]= temp
+
+def inPlaceMerge(nums,start, end):
+
+    gap = end - start + 1
+    gap = gaps(gap)
+
+    while gap > 0:
+        i = start
+        while (i + gap) <= end:
+            j = i + gap
+
+            if nums[i] > nums[j]:
+                swap(nums, i, j)
+
+            i += 1
+
+        gap = gaps(gap)
+
+def ms(arr, start, end):
+    if start == end:
+        return
+    mid = (start + end) // 2
+    ms(arr,start, mid )
+    ms(arr, mid+1, end)
+
+    inPlaceMerge(arr,start,end)
+
+#in place ms
 def merge_sort(arr,cmp):
+    ms(arr, 0, len(arr) - 1)
     pass
 
 # must be in-place sort
